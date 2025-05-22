2cfb37aabe19baa59d53ee97bb4109c3392e6bca
Marshallhch <marshallhch@gmail.com>
2025-05-22 13:12:40 +0900

    '0522_v1.5'

--- APPs/graph_app/front/src/components/baseLayout/Appbar.jsx
+++ APPs/graph_app/front/src/components/baseLayout/Appbar.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { MdOutlineMenu } from 'react-icons/md';
+import { useDispatch } from 'react-redux';
+import { setSidebarOpen } from '../../redux/slices/sidebarSlice';
 
 const Appbar = () => {
-  return <div>Appbar</div>;
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <button onClick={() => dispatch(setSidebarOpen())}>
+        <MdOutlineMenu />
+      </button>
+    </div>
+  );
 };
 
 export default Appbar;
