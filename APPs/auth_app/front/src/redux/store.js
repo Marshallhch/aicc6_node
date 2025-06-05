9f405b7e96e4279e9d2b73726b76e3f07e191403
Marshallhch <marshallhch@gmail.com>
2025-06-05 15:40:05 +0900

    '0605_auth_1.3'

--- APPs/auth_app/front/src/redux/store.js
+++ APPs/auth_app/front/src/redux/store.js
@@ -1,9 +1,11 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlices';
+import loginReducer from './slices/loginSlices';
 
 const store = configureStore({
   reducer: combineReducers({
     auth: authReducer,
+    login: loginReducer,
   }),
 });
 
